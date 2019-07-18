9db7de29229ab44b2523e80663dc3b0abe5519ac
c568254965 <568254965@qq.com>
2019-07-18 14:33:35 +0800

    777

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 git pull
 mvn clean package -Dmaven.test.skip=true
 
-docker rm -f eureka-center:test
+docker rm -f eureka-center
 docker rmi -f eureka-center:test
 docker build -t eureka-center:test .
 docker run -d -p 6801:6801 --name eureka-center eureka-center:test java -Dspring.profiles.active=test -jar /app/app.jar
