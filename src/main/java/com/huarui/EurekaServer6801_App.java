d2273f2b6c5d07e4eeca6ed128711dc9c7e9d72f
c568254965 <568254965@qq.com>
2019-07-23 16:45:18 +0800

    fix

--- src/main/java/com/huarui/EurekaServer6801_App.java
+++ src/main/java/com/huarui/EurekaServer6801_App.java
@@ -4,8 +4,6 @@ package com.huarui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import zipkin.server.internal.EnableZipkinServer;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
