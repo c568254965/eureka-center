ca68f1183fb8b2859e181e764c1aff30d3670920
c568254965 <568254965@qq.com>
2019-07-25 17:23:50 +0800

    ha

--- src/main/java/com/huarui/EurekaServer6801_App.java
+++ src/main/java/com/huarui/EurekaServer6801_App.java
@@ -3,10 +3,12 @@ package com.huarui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+@EnableDiscoveryClient
 public class EurekaServer6801_App
 {
 	public static void main(String[] args)
