3ecce4cdb3c2a96f41a98d4a6e4f3db2d5512391
c568254965 <568254965@qq.com>
2019-07-26 11:28:14 +0800

    .

--- run2.sh
+++ run2.sh
@@ -6,4 +6,4 @@ mvn clean package -Dmaven.test.skip=true
 docker rm -f eureka-center2
 docker rmi -f eureka-center2:test
 docker build -f Dockerfile2 -t eureka-center2:test .
-docker run -d -p 6802:6802 --name eureka-center2 eureka-center2:test java -Dspring.profiles.active=test2 -jar /app/app.jar
+docker run -d -p 6802:6802  --add-host csrr-eureka:140.143.227.51  --name eureka-center2 eureka-center2:test java -Dspring.profiles.active=test2 -jar /app/app.jar
